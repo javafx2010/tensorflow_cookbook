524426fa2516948cf53b80435a7fdd26a8fa7199
javafx <javafx2010@icloud.com>
2018-01-07 21:07:27 +0800

    remove unuse code

--- 01_Introduction/02_Creating_and_Using_Tensors/02_tensors.py
+++ 01_Introduction/02_Creating_and_Using_Tensors/02_tensors.py
@@ -58,10 +58,6 @@ rnorm_var = tf.random_normal([row_dim, col_dim], mean=0.0, stddev=1.0)
 # Add summaries to tensorboard
 merged = tf.summary.merge_all()
 
-# Initialize graph writer:
-
-writer = tf.summary.FileWriter("/tmp/variable_logs", graph=sess.graph)
-
 # Initialize operation
 initialize_op = tf.global_variables_initializer()
 
